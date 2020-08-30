77f4b078337324855983e16ed13a15a36ef18103
MUSA FURKAN ZENBİLCİ <69345095+k4boom@users.noreply.github.com>
2020-08-30 12:25:07 +0300

    Update bla.cpp

--- bla.cpp
+++ bla.cpp
@@ -4,6 +4,8 @@
 #include<stdio.h>
 #include<string>
 using namespace std;
+string strdivision(string divident,string divider);
+string strsubstract(string from,string that);
 void reverseStr(string& str) //string reverser function
 { 
     int n = str.length();  
@@ -59,14 +61,11 @@ string addstr(string x,string z){			//function to   add numbers in string form
 }
 int main(){
     string a,b;
-    b="6402373705728000";
-    cout<<multiplestr(b,"19");
-    //while(1){
-    //	cin>>a;
-    //	if(a=="q") break;
-    //	b=multiplestr(a,b);
-    //	cout<<b;
-}//}
+    cin>>a>>b;
+    cout<<strdivision(a,b);
+
+    
+}
 string multiplestr(string a,string b){
     string z,x;
     if(a.length()>b.length()){z=a;x=b;}else{z=b;x=a;}
@@ -113,4 +112,122 @@ string multiplestr(string a,string b){
     	result=addstr(result,carp[i]);
     }
     return result;
+}
+string strsubstract(string from,string that){
+    //find which one is bigger
+    //TODO: create a more complicated algorithm for the numbers with the same number of digits
+
+    string big,small;
+    if(from.length()>=that.length()) {big=from;small=that;}
+    else{ big=that;small=from;}
+    
+    //reverse the stringsr
+    reverseStr(big);reverseStr(small);
+    
+    //find the gap
+    string result,gap;
+    //substract the digits from each other with the same index
+    for(int a=0;a<small.length();a++){
+        int bigholder,smallholder,sumholder,extraholder;
+        bigholder=big[a]-48;
+        smallholder=small[a]-48;
+       
+        
+        //if digit of big is smaller,take 1 from the next digit of the big
+        if(smallholder>bigholder){
+            
+            extraholder=big[a+1]-48;
+            big[a+1]=(extraholder-1)+48;
+            bigholder+=10;
+        } 
+        sumholder=bigholder-smallholder;
+        gap=to_string(sumholder)+gap;
+    }
+   
+    //let the digits that left fall down to the result 
+    for(int i=small.length();i<big.length();i++){
+        gap=big[i]+gap;
+    }
+   
+    //reverseStr(gap);
+    result=gap;
+    //delete the zeros at the beginning
+    while(result[0]=='0'){
+        result.erase(0,1);
+    }
+    //return the result with minus or not
+     
+    if(from.length()>=that.length()) return result;
+    return "-"+result;
+}
+//TODO: Create a compare function
+bool isBig(string supposedtobeBig,string supposedtobeSmall){
+
+//digit camparison
+    if(supposedtobeBig.length()>supposedtobeSmall.length()){
+        return 1;
+    }
+    else if(supposedtobeBig.length()<supposedtobeSmall.length()){
+        return 0;
+    }
+//if equal:for length 
+    else{
+        int i;
+        for(i=0;i<supposedtobeSmall.length();i++){
+            int bigholder,smallholder;
+            bigholder=supposedtobeBig[i]-48;
+            smallholder=supposedtobeSmall[i]-48;
+            if(bigholder>smallholder){
+                return 1;break;
+            }
+            else if(bigholder<smallholder){
+                return 0;break;
+            }
+            
+        }
+        if(i==supposedtobeSmall.length()){
+            return 0;
+        }
+    }
+//from the beginning compare each digit until one of them is bigger
+}
+//TODO: Create a division functıon
+string strdivision(string divident,string divider){
+//Compare the divident and divider and if divider is bigger quit
+if(isBig(divider,divident)){
+    cout<<"Divider may not be bigger than divident for this program"<<endl;
+    exit(EXIT_SUCCESS);
+}
+string result="0";
+//Reverse the strings
+reverseStr(divident);reverseStr(divider);
+//Compare the first digits of the bölen(divider) and bölünen(divident)
+//if the first digit of the divident is bigger than divider:
+while(isBig(divident,divider)){
+    int bigholder,smallholder,extraholder;
+    bigholder=divident[0]-48;
+    smallholder=divider[0]-48;
+    if(bigholder>=smallholder){
+        extraholder=bigholder/smallholder;
+        string extra,sideresult;
+        extra=to_string(extraholder);
+        sideresult=multiplestr(extra,divider);
+        while(isBig(sideresult,divident)){
+        extraholder-=1;
+        extra=to_string(extraholder);
+        sideresult=multiplestr(extra,divider);
+        }  
+        result=addstr(result,extra);
+        divident=strsubstract(divident,sideresult);
+        cout<<divident<<endl;  
+    }return result;
+}
+//  divide the digits and multiply the result with the digits of the divider
+//  if the result of the multiplication is bigger than divident multiply with 1 less and substract it from the divident
+//  add the result to the quotient as addition
+//  keep doing this until the divident is less than divider
+//if not:
+//  get one more digit from divident
+//  divide
+//after substraction drop one digit from the divident if there is
 }
